af4703e3806d06e88107f30d9255976a0d359070
Sergei <arcivus@gmail.com>
2016-09-13 08:13:05 +0300

    change port for prod

--- server/app.js
+++ server/app.js
@@ -70,5 +70,5 @@ io.on('connection', function(socket) {
 
 // Start server
 server.listen(app.get('port'), function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+  console.log('Express server listening on port %d in %s mode', (process.env.PORT || app.get('port')), app.get('env'));
 });
